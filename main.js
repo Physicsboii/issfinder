6749700545c9f5b4cc3c46abd798ff3d9004b570
Debarghya Samanta <39517183+debarghya2006@users.noreply.github.com>
2021-03-31 15:49:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const api = `http://api.open-notify.org/iss-now.json`;
-
+const api = `https://api.wheretheiss.at/v1/satellites/25544`;
 
 fetch(api)
   .then((response) => {
@@ -9,7 +8,7 @@ fetch(api)
     const long = document.querySelector(".longitude");
     const lat = document.querySelector(".latitude");
     console.log(data);
-    const { latitude, longitude } = data.iss_position;
+    const { latitude, longitude } = data;
     console.log("THE LATITUDE OF ISS IS => " + latitude);
     console.log("THE LONGITUDE OF ISS IS => " + longitude);
     long.innerText = longitude;
